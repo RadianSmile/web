2fcd6324b22689851c746b5160b39c27ecae9211
Radian <craziestdavid@gmail.com>
2013-07-07 10:15:12 +0800

    good

---
+++ Test/2013.7.7_count.php
@@ -0,0 +1,14 @@
+<?php 
+$a = $_SERVER['REMOTE_ADDR'];
+?><!doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>無標題文件</title>
+</head>
+
+<body>
+<p><?php echo $a;?></p>
+
+</body>
+</html>
